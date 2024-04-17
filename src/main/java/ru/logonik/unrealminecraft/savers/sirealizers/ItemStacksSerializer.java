05dc85f4b98396a0a0b9e31c0c7795a9d16f4e9d
LogonikGG <reglogonik@mail.ru>
2024-04-17 17:49:40 +0500

    bug fix, serialization itemstacks

--- src/main/java/ru/logonik/unrealminecraft/savers/sirealizers/ItemStacksSerializer.java
+++ src/main/java/ru/logonik/unrealminecraft/savers/sirealizers/ItemStacksSerializer.java
@@ -2,32 +2,33 @@ package ru.logonik.unrealminecraft.savers.sirealizers;
 
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import ru.logonik.unrealminecraft.util.Util;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class ItemStacksSerializer implements JsonSerializer<List<ItemStack>>, JsonDeserializer<List<ItemStack>> {
     @Override
     public JsonElement serialize(List<ItemStack> src, Type typeOfSrc, JsonSerializationContext context) {
-        Type token = new TypeToken<Map<String, Object>>() {}.getType();
-        JsonArray array = new JsonArray();
-        for (ItemStack itemStack : src) {
-            array.add(context.serialize(itemStack.serialize(), token));
-        }
-        return array;
+        ItemStack[] itemStacks = src.toArray(src.toArray(new ItemStack[0]));
+        String s = Util.itemStackArrayToBase64(itemStacks);
+        return new JsonPrimitive(s);
     }
 
     @Override
     public List<ItemStack> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonArray asJsonArray = json.getAsJsonArray();
-        ArrayList<ItemStack> result = new ArrayList<>();
-        Type token = new TypeToken<Map<String, Object>>() {}.getType();
-        for (JsonElement jsonElement : asJsonArray) {
-            result.add(ItemStack.deserialize(context.deserialize(jsonElement, token)));
+        String string = json.getAsString();
+        try {
+            ItemStack[] itemStacks = Util.itemStackArrayFromBase64(string);
+            return new ArrayList<>(Arrays.asList(itemStacks));
+        } catch (IOException e) {
+            throw new JsonParseException(e);
         }
-        return result;
     }
 }
--- src/main/java/ru/logonik/unrealminecraft/util/Util.java
+++ src/main/java/ru/logonik/unrealminecraft/util/Util.java
@@ -1,8 +1,15 @@
 package ru.logonik.unrealminecraft.util;
 
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 import ru.logonik.unrealminecraft.arenasmodels.SpawnPointAbstract;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -49,5 +56,45 @@ public class Util {
                     + " Заспавнен: " + point.isSpawned());
         }
         return joiner.toString();
+    }
+
+
+    public static String itemStackArrayToBase64(ItemStack[] items) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            // Write the size of the inventory
+            dataOutput.writeInt(items.length);
+
+            // Save every element in the list
+            for (int i = 0; i < items.length; i++) {
+                dataOutput.writeObject(items[i]);
+            }
+
+            // Serialize that array
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
+
+    public static ItemStack[] itemStackArrayFromBase64(String data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack[] items = new ItemStack[dataInput.readInt()];
+
+            // Read the serialized inventory
+            for (int i = 0; i < items.length; i++) {
+                items[i] = (ItemStack) dataInput.readObject();
+            }
+
+            dataInput.close();
+            return items;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
     }
 }
